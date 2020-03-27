319902d8c82990dd6a2a071f13859ef29e6678d7
João Manoel Neto <joaaomanooel@gmail.com>
2020-03-27 13:43:14 -0300

    update gulp scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
-const gulp = require('gulp');
-const clean = require('gulp-clean');
+const { src, series, dest, watch: gulpWatch } = require('gulp');
+const gulpClean = require('gulp-clean');
 const ts = require('gulp-typescript');
+
+const JSON_FILES = 'src/**/*.json';
+const TS_FILES = 'src/**/*.ts';
 
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('scripts', () => tsProject
-  .src()
-  .pipe(tsProject())
-  .js.pipe(gulp.dest('dist'))
-);
+const scripts = () => tsProject.src().pipe(tsProject()).js.pipe(dest('dist'));
+const clean = () => src('dist', { allowEmpty: true }).pipe(gulpClean());
+const handleStaticFiles = () => src([JSON_FILES]).pipe(dest('dist'));
+const watcher = () => gulpWatch([TS_FILES, JSON_FILES], build());
+const build = () => series(clean, handleStaticFiles, scripts);
+const watch = () => series(build(), watcher);
+
+exports.watch = watch();
+exports.build = build();
+exports.default = watch();
