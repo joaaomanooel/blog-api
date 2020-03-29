064bdad4ad358d17d9b574d247277d10f4170baf
João Manoel Neto <joaaomanooel@gmail.com>
2020-03-28 21:17:54 -0300

    add create user mutation

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -15,11 +15,22 @@ const typeDefs = `
   type Query {
     allUsers: [User!]!
   }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User
+  }
 `;
 
 const resolvers = {
   Query: {
     allUsers: (): any[] => users,
+  },
+  Mutation: {
+    createUser: (_parent, args): any => {
+      const newUser = { id: users.length + 1, ...args };
+      users.push(newUser);
+      return newUser;
+    },
   },
 };
 
