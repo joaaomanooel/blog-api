c03b0f70fdc44af28e795830172b5ca498f26d31
João Manoel Neto <joaaomanooel@gmail.com>
2020-04-03 16:53:24 -0300

    add sequelize at startup api

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,15 @@ require('dotenv').config();
 
 import * as http from 'http';
 import app from './app';
+import db from './models';
+
 import { normalizePort, onError, onListening } from './utils';
 
 const server = http.createServer(app);
 const port = normalizePort(process.env.PORT || 5000);
 
-server.listen(port);
-server.on('error', e => onError(e));
-server.on('listening', onListening(server));
+db.sequelize.sync().then(() => {
+  server.listen(port);
+  server.on('error', e => onError(e));
+  server.on('listening', onListening(server));
+});
